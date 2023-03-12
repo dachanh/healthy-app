d7f94636646ff6a8b5763f42d53bc9f52a28cd32
dachanh <ghostcodert7@gmail.com>
2023-03-12 17:58:08 +0700

    wip

---
+++ config/init/init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=0 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE "${ENV}-healthy-arent" ENCODING 'UTF8' TEMPLATE template0;
+    CREATE DATABASE "test-healthy-arent" ENCODING 'UTF8' TEMPLATE template0;
+EOSQL
